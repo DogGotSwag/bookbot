82e5297cfc079582bfd7943c66d7dd4e6008e862
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-07-12 20:26:39 -0600

    make get_num_of_characters feature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import num_of_words
+from stats import get_num_of_characters
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -9,6 +10,8 @@ def get_book_text(file_path):
 def main():
     text = get_book_text('./books/frankenstein.txt')
     num_of_word_in_book = num_of_words(text)
-    print(f"{num_of_word_in_book} words found in the document")
+    # print(f"{num_of_word_in_book} words found in the document")
+    char_dict = get_num_of_characters(text)
+    print(char_dict)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,17 @@
 def num_of_words(text):
     words_array = text.split()
     return len(words_array)
+
+def get_num_of_characters(text):
+    dict = {}
+
+    symbols_and_characters = list(text)
+    for char in symbols_and_characters:
+        lower_char = char.lower()
+        if lower_char in dict:
+            dict[lower_char] = dict[lower_char] + 1
+        else:
+            dict[lower_char] = 1
+
+    return dict
+
