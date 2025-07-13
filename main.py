1cc88fcbfdfdd0944c1b6280afab566e2eb8553c
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-07-12 23:23:27 -0600

    create book report inside the main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from stats import num_of_words
-from stats import get_num_of_characters
+from stats import get_num_of_characters, sort_dict
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -10,8 +10,19 @@ def get_book_text(file_path):
 def main():
     text = get_book_text('./books/frankenstein.txt')
     num_of_word_in_book = num_of_words(text)
-    # print(f"{num_of_word_in_book} words found in the document")
     char_dict = get_num_of_characters(text)
-    print(char_dict)
+    sorted_dict = sort_dict(char_dict)
+
+    print('============ BOOKBOT ============')
+    print('Analyzing book found at books/frankenstein.txt...')
+    print('----------- Word Count ----------')
+    print(f"Found {num_of_word_in_book} total words")
+    print('--------- Character Count -------')
+    for key in sorted_dict:
+        if key["char"].isalpha():
+            print(f"{key["char"]}: {key["num"]}")
+    print('============= END ===============')
+
+
 
 main()
