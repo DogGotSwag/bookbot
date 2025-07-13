2aee6be63207cd37d1658df7fe1fc38353de272c
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-07-12 19:29:34 -0600

    create num_of_words feature
    returns number of words in a text

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
         return file_contents
-
+    
+def num_of_words(text):
+    words_array = text.split()
+    return len(words_array)
 
 def main():
     text = get_book_text('./books/frankenstein.txt')
-    print(text)
+    num_of_word_in_book = num_of_words(text)
+    print(num_of_word_in_book)
 
 main()
