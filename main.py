958c18d00e09edefe4a062ff9d44345a42383435
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-07-12 23:33:01 -0600

    allow CLI args instead of hardcoded file paths

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from stats import num_of_words
-from stats import get_num_of_characters, sort_dict
+from stats import get_num_of_characters, sort_dict, num_of_words
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -8,7 +8,11 @@ def get_book_text(file_path):
     
 
 def main():
-    text = get_book_text('./books/frankenstein.txt')
+    if len(sys.argv) < 2:
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
+
+    text = get_book_text(sys.argv[1])
     num_of_word_in_book = num_of_words(text)
     char_dict = get_num_of_characters(text)
     sorted_dict = sort_dict(char_dict)
@@ -22,7 +26,5 @@ def main():
         if key["char"].isalpha():
             print(f"{key["char"]}: {key["num"]}")
     print('============= END ===============')
-
-
 
 main()
